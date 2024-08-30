ad3b28ccf8b0fc9636fb4e292ebf7a5366fb135e
Kush Vasaniya <vasaniyakush@gmail.com>
2024-08-30 21:09:03 +0530

    added models
    Signed-off-by: Kush Vasaniya <vasaniyakush@gmail.com>

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Chapter(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Exam(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()                                       
