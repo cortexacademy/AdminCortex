952c8690fd0e933358c65ff80a919890999ecb85
Kush Vasaniya <vasaniyakush@gmail.com>
2024-08-30 21:22:21 +0530

    updated env management

--- admin_panel/settings.py
+++ admin_panel/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
@@ -78,11 +79,11 @@ WSGI_APPLICATION = 'admin_panel.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "AXON",
-        "USER": "postgres",
-        "PASSWORD": "1234",
-        "HOST": "127.0.0.1",
-        "PORT": "5433",
+        "NAME": os.getenv("DB_NAME",""),
+        "USER": os.getenv("DB_USER",""),
+        "PASSWORD": os.getenv("DB_PASS",""),
+        "HOST": os.getenv("DB_HOST",""),
+        "PORT": os.getenv("DB_PORT",""),
     }
 }
 
